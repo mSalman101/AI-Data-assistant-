188cce89bef2756db66efd31cc48ecab17177ec9
Mohammad Salman Ansari <mohd.salman01a@gmail.com>
2025-06-24 13:34:25 +0530

    improved UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from "react";
 import axios from "axios";
 import "./App.css";
+import ReactMarkdown from 'react-markdown';
+
+const apiKey = import.meta.env.VITE_API;
+
 
 function App() {
   const [question, setQuestion] = useState("");
-  const [response, setResponse] = useState("");
   const [ans, setAns] = useState("");
 
   async function getAnswer() {
     console.log("loading...");
-    const condition =
-      "You are an AI assistant designed **ONLY** for data analysis. You will **NOT** answer any questions that are unrelated to data analysis. If asked about anything else, politely respond: I can only help with data analysis tasks.";
-    console.log(condition + question);
+    
+    console.log( question);
     const response = await axios({
-      url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${
-        import.meta.env.VITE_API
-      }`,
+      url: `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${apiKey}`,
       method: "post",
       data: {
         contents: [
@@ -28,15 +28,15 @@ function App() {
     const ans =
       response["data"]["candidates"][0]["content"]["parts"][0]["text"];
     setAns(ans);
-    console.log(ans + "Grateful");
+    console.log(ans);
   }
 
   return (
     <>
       <h1 className="head1">Your AI Assistant</h1>
-      <h4 className="head2">{ans || "How may I assist You "}</h4>
+      <h2 className="head2">{<ReactMarkdown>{ans}</ReactMarkdown>|| "How may I assist You "}</h2>
       <div className="box">
-        <div className="input-group mb-3">
+        <div className="input-group py-3 my-4">
           <textarea
             className="form-control"
             id="questionInput"
@@ -47,13 +47,22 @@ function App() {
           <button
             type="button"
             id="get"
-            className="btn btn-secondary"
+            className="btn btn-secondary bg-indigo-600 hover:bg-indigo-700 transition duration-200 text-white rounded-r-xl px-4 py-2
+"
             onClick={getAnswer}
           >
-            Ask!
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="26"
+              height="26"
+              fill="white"
+              className="bi bi-send"
+              viewBox="0 0 16 16"
+            >
+              <path d="M15.854.146a.5.5 0 0 1 .11.54l-5.819 14.547a.75.75 0 0 1-1.329.124l-3.178-4.995L.643 7.184a.75.75 0 0 1 .124-1.33L15.314.037a.5.5 0 0 1 .54.11ZM6.636 10.07l2.761 4.338L14.13 2.576zm6.787-8.201L1.591 6.602l4.339 2.76z" />
+            </svg>
           </button>
         </div>
-        <p>Response: {response}</p>
       </div>
     </>
   );
